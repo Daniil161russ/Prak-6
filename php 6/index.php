96e56d0e7abc9cefd74514f23de2077be4b9ef47
Daniil <43216069+Daniil161russ@users.noreply.github.com>
2019-04-03 14:08:43 +0300

    Add files via upload

---
+++ php 6/index.php
@@ -0,0 +1,68 @@
+<?php
+
+	class User 
+	{
+		public $name;
+		public $surname;
+		function __construct($userName,$userSurName)
+		{
+			$this -> name = $userName;
+			$this -> surname = $userSurName;
+		}
+		public function getName(){
+			return $this -> name;
+		}
+		public function getSurName(){
+			return $this -> surname;
+		}
+		public function getStudentName(){
+			echo $this -> name ," ", $this -> surname;
+		}
+	}
+
+	class Student extends User
+	{
+		public $year;
+		function __construct($userName,$userSurName,$studentYear)
+		{
+			parent::__construct($userName,$userSurName);
+			$this -> year = $studentYear;
+		}
+		public function getYear(){
+			return $this -> year;
+		}
+		public function getStudentName(){
+			echo $this -> name ," ", $this -> surname;
+		}
+		public function getCourse(){
+			$course = date("Y") - $this -> year;
+			if ($course == 1) {
+				echo "Первый курс";
+			}
+			elseif ($course == 2) {
+				echo "Второй курс";
+			}
+			elseif ($course == 3) {
+				echo "Третий курс";
+			}
+			elseif ($course == 4) {
+				echo "Четвёртый курс";
+			}
+			elseif ($course == 5) {
+				echo "Пятый курс";
+			}
+			elseif ($course > 5) {
+				echo "Закончил обучаться";
+			}
+			elseif ($course <= 0) {
+				echo "Еще не поступил";
+			}
+		}
+	}
+	$firstStud = new Student("Daniil","Boyko",2016);
+	$firstStud -> getStudentName();
+	echo "<br>";
+	$firstStud -> getCourse();
+
+	// var_dump($firstStud);
+  ?>
